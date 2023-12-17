ccb329d796f72504df40f7c8103864a1b62b27dd
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-12-17 15:42:35 +0100

    Update Jam.sh

--- Jam.sh
+++ Jam.sh
@@ -1,6 +1,16 @@
-# Kontrola či je skript spustený so sudo
-if [ "$EUID" -ne 0 ]; then
-  echo "Cant start.\nPlease start it with sudo ./Jam.sh"
+#!/bin/bash
+
+# Check if the script is run with sudo
+if [ "$(id -u)" -ne 0 ]; then
+  echo "------------------------------------------------------------" | lolcat
+  echo "[+] \e[31mCANT START WITHOUT SUDO PERMISSION\e[0m [+]" | lolcat
+  echo "[+] Please start this script with                       [+]" | lolcat
+  echo "[+]         --> \e[31msudo ./Jam.sh\e[0m                           [+]" | lolcat
+  echo "------------------------------------------------------------" | lolcat
   exit 1
 fi
 
+# Your existing script logic goes here for the part that follows when executed with sudo
+
+echo "Script is running with sudo permission." | lolcat
+# Continue your script execution here
